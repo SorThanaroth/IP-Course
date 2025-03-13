c3f05ebbd8076da7855806a58918e7cdc5259180
SorThanaroth <sorthanaroth018@gmail.com>
2025-03-13 22:42:28 +0700

    add: � step 1 - step 6 done

---
+++ laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Get /api/Products.
+     */
+    public function getProducts()
+    {
+        $products = Product::all();
+        return response()->json([
+            "message" => "Products retrieved successfully",
+            "data" => $products
+        ]);
+        // return ["message" => "Getting list of Product"];
+    }
+
+    /**
+     * Post /api/Products.
+     */
+    public function createProduct(Request $request)
+    {
+        // $product = Product::create([
+        //     'name' => $request->input('name')
+        // ]);
+        // $product = Product::create(['name' => 'Book']);
+        $product = DB::table('products')->insert(['name' => 'Book']);
+
+
+        
+        return response()->json([
+            "message" => "Product created successfully",
+            "data" => $products
+        ], 201);
+    }
+
+    /**
+     * Get /api/Products/{ProductId}.
+     */
+    public function getProduct($productId)
+    {
+        return ["message" => "Getting 1 Product base on given ProductId"];
+    }
+
+    /**
+     * Patch /api/Products/{ProductId}.
+     */
+    public function updateProduct($productId)
+    {
+        return ["messsage" => "Updating 1 Product base on given ProductId"];
+    }
+
+    /**
+     * Delete /api/Products/{ProductId}.
+     */
+    public function deleteProduct($productId)
+    {
+        return ["message" => "Deleting 1 Product base on given ProductId"];
+    }
+}
